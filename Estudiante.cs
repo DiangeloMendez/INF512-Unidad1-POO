7b9173234759600b748b68130f6b392961575324
DiangeloMendez <diangelomendezmejia@gmail.com>
2026-02-14 16:16:21 -0500

    Create Estudiante.cs
    Tercer commit: agregado método InscribirseMateria

---
+++ Estudiante.cs
@@ -0,0 +1,8 @@
+public void Estudiar()
+{
+    Console.WriteLine(nombre + " está estudiando POO.");
+}
+public void InscribirseMateria(string materia)
+{
+    Console.WriteLine(nombre + " se ha inscrito en la materia: " + materia);
+}
